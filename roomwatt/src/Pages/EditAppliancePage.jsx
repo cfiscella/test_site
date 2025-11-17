f8651f7d527ebc1dcb0f4efdaff1d6ee99ef5fc3
Aaron <Ac76268n@pace.edu>
2025-11-17 01:26:59 -0500

    continuing the edit page

--- roomwatt/src/Pages/EditAppliancePage.jsx
+++ roomwatt/src/Pages/EditAppliancePage.jsx
@@ -1,3 +1,27 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import DeleteButton from "../components/DeleteButton";
+import { RoomContext } from "../RoomContext";
+
+export default function EditRoom() {
+    const { state } = useLocation(); // room data passed from RoomList
+    const navigate = useNavigate();
+  
+    const { updateApplianceWatts, deleteAppliance, deleteRoom } =
+    useContext(RoomContext);
+    
+    const room = state;
+
+    const handleWattChange = (applianceId, value) => {
+    updateApplianceWatts(room.id, applianceId, value);
+  };
+
+  const handleDeleteAppliance = (applianceId) => {
+    deleteAppliance(room.id, applianceId);
+  };
+  const handleDeleteRoom = () => {
+    deleteRoom(room.id);
+    navigate("/");
+  };
+  
+}
